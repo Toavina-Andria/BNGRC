0f8e557cd17456d30079b233f56c440ecee16a7e
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-02-16 15:09:29 +0300

    Update routes.php
    route vide

--- app/config/routes.php
+++ app/config/routes.php
@@ -10,11 +10,27 @@ use flight\net\Router;
 
 /**
  * @var Router $router
+/**
+ * @var Router $router
  * @var Engine $app
  */
 
+// This wraps all routes in the group with the SecurityHeadersMiddleware
+$router->group('', function (Router $router) {
 $router->group('', function(Router $router) use ($app) {
 
+	$router->get('/', function () {
+		// Render the home page view (views/home.php)
+	});
+
+	$router->get('/hello-world/@name', function ($name) {
+		echo '<h1>Hello world! Oh hey ' . $name . '!</h1>';
+	});
+
+	$router->group('/api', function () use ($router) {
+		$router->get('/users', [ApiExampleController::class, 'getUsers']);
+		$router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
+		$router->post('/users/@id:[0-9]', [ApiExampleController::class, 'updateUser']);
 	$router->group('/besoins', function() use ($router, $app) {
 
 		// Liste des besoins
@@ -26,4 +42,4 @@ $router->group('', function(Router $router) use ($app) {
 
 	});
 
-}, [ SecurityHeadersMiddleware::class ]);
+}, [SecurityHeadersMiddleware::class]);
