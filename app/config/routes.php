fcb14314c91eac6947292254a3b492a465550dc9
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-02-17 02:34:32 +0300

    router update

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,6 +5,7 @@ use app\controllers\BesoinController;
 use app\controllers\DonController;
 use app\controllers\DashboardController;
 use app\controllers\VilleController;
+use app\controllers\InsertionController;
 use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
@@ -14,21 +15,21 @@ use flight\net\Router;
  * @var Engine $app
  */
 
-$router->group('', function(Router $router)  {
+$router->group('', function (Router $router) {
 
 	$router->get('/', [DashboardController::class, 'index']);
 
-	$router->group('/villes', function() use ($router) {
+	$router->group('/villes', function () use ($router) {
 		// Voir les besoins d'une ville
 		$router->get('/besoins', [VilleController::class, 'showVilleBesoins']);
 	});
 
-	$router->group('/besoins', function() use ($router) {
+	$router->group('/besoins', function () use ($router) {
 		// Liste des besoins
-			$router->get('/liste', [BesoinController::class, 'getAllBesoins']);
+		$router->get('/liste', [BesoinController::class, 'getAllBesoins']);
 	});
 
-	$router->group('/dons', function() use ($router) {
+	$router->group('/dons', function () use ($router) {
 		// Liste des dons
 		$router->get('/liste', [DonController::class, 'listeDons']);
 		// formulaire d'ajout de don
@@ -38,17 +39,17 @@ $router->group('', function(Router $router)  {
 		$router->get('/dispatch', [DonController::class, 'dispatchDons']);
 	});
 
-	$router->group('/sinistres', function() use ($router) {
-				// Liste des sinistres
+	$router->group('/sinistres', function () use ($router) {
+		// Liste des sinistres
 		$router->get('/liste', [SinistreController::class, 'getAllSinistres']);
 		// formulaire d'ajout de sinistre (GET) et traitement (POST)
-		$router->get('/insert', [\app\controllers\InsertionController::class, 'showSinistreForm']);
-		$router->post('/insert', [\app\controllers\InsertionController::class, 'insertSinistre']);
-			// routes pour besoins rattachés aux sinistres
-		$router->group('/besoins', function() use ($router) {
-				$router->get('/insert', [\app\controllers\InsertionController::class, 'showBesoinForm']);
-				$router->post('/insert', [\app\controllers\InsertionController::class, 'insertBesoin']);
+		$router->get('/insert', [InsertionController::class, 'showSinistreForm']);
+		$router->post('/insert', [InsertionController::class, 'insertSinistre']);
+		// routes pour besoins rattachés aux sinistres
+		$router->group('/besoins', function () use ($router) {
+			$router->get('/insert', [InsertionController::class, 'showBesoinForm']);
+			$router->post('/insert', [InsertionController::class, 'insertBesoin']);
 		});
 	});
 
-}, [ SecurityHeadersMiddleware::class ]);
+}, [SecurityHeadersMiddleware::class]);
