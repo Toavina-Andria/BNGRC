1cd8a703d0e9b6e16c03650c2a90ddc666c1d9b5
nekena <nekenabrunella@gmail.com>
2026-02-17 00:24:19 +0300

    v2 script

---
+++ script-database/2026-2-17_00:23-script.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE bngrc;
+USE bngrc;
+
+CREATE TABLE IF NOT EXISTS bn_region (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+CREATE TABLE IF NOT EXISTS bn_ville (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL,
+    population INT NOT NULL,
+    id_region INT,
+    FOREIGN KEY (id_region) REFERENCES bn_region(id)
+);
+
+CREATE TABLE IF NOT EXISTS bn_sinistre (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre_sinistres INT NOT NULL,
+    id_ville INT,
+    FOREIGN KEY (id_ville) REFERENCES bn_ville(id)
+);
+
+CREATE TABLE IF NOT EXISTS bn_categorie_besoin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS bn_sinistre_besoin (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_sinistre INT,
+    id_categorie_besoin INT,
+    description VARCHAR(255),
+    quantite INT NOT NULL,
+    prix_unitaire DECIMAL(10, 2),
+    FOREIGN KEY (id_sinistre) REFERENCES bn_sinistre(id),
+    FOREIGN KEY (id_categorie_besoin) REFERENCES bn_categorie_besoin(id)
+);
+
+CREATE TABLE bn_don (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    type ENUM('argent', 'nature') NOT NULL,
+    donateur VARCHAR(100),
+    date_don DATETIME NOT NULL,
+    id_ville INT,
+    FOREIGN KEY (id_ville) REFERENCES bn_ville(id)
+);
+
+CREATE TABLE bn_don_argent (
+    id_don INT PRIMARY KEY,
+    montant DECIMAL(12,2) NOT NULL,
+    montant_restant DECIMAL(12,2) NOT NULL,
+    FOREIGN KEY (id_don) REFERENCES bn_don(id)
+);
+
+CREATE TABLE bn_don_nature (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_don INT,
+    id_categorie_besoin INT,
+    description VARCHAR(255),
+    quantite INT NOT NULL,
+    FOREIGN KEY (id_don) REFERENCES bn_don(id),
+    FOREIGN KEY (id_categorie_besoin) REFERENCES bn_categorie_besoin(id)
+);
+
+CREATE TABLE bn_achat (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    id_don_argent INT,
+    id_sinistre_besoin INT,
+    quantite INT NOT NULL,
+    prix_unitaire DECIMAL(10,2) NOT NULL,
+    frais_pourcentage DECIMAL(5,2) NOT NULL,
+    montant_total DECIMAL(12,2) NOT NULL,
+    date_achat DATETIME NOT NULL,
+    FOREIGN KEY (id_don_argent) REFERENCES bn_don_argent(id_don),
+    FOREIGN KEY (id_sinistre_besoin) REFERENCES bn_sinistre_besoin(id)
+);
