eef140b1c3ea06f5bb35b807dbf77815e90551b0
AikyDJ <96826093+AikyDJ@users.noreply.github.com>
2026-02-16 15:09:29 +0300

    Update routes.php
    route vide

--- app/config/routes.php
+++ app/config/routes.php
@@ -5,26 +5,26 @@ use app\middlewares\SecurityHeadersMiddleware;
 use flight\Engine;
 use flight\net\Router;
 
-/** 
- * @var Router $router 
+/**
+ * @var Router $router
  * @var Engine $app
  */
 
 // This wraps all routes in the group with the SecurityHeadersMiddleware
-$router->group('', function(Router $router) use ($app) {
+$router->group('', function (Router $router) {
 
-	$router->get('/', function() use ($app) {
-		$app->render('welcome', [ 'message' => 'You are gonna do great things!' ]);
+	$router->get('/', function () {
+		// Render the home page view (views/home.php)
 	});
 
-	$router->get('/hello-world/@name', function($name) {
-		echo '<h1>Hello world! Oh hey '.$name.'!</h1>';
+	$router->get('/hello-world/@name', function ($name) {
+		echo '<h1>Hello world! Oh hey ' . $name . '!</h1>';
 	});
 
-	$router->group('/api', function() use ($router) {
-		$router->get('/users', [ ApiExampleController::class, 'getUsers' ]);
-		$router->get('/users/@id:[0-9]', [ ApiExampleController::class, 'getUser' ]);
-		$router->post('/users/@id:[0-9]', [ ApiExampleController::class, 'updateUser' ]);
+	$router->group('/api', function () use ($router) {
+		$router->get('/users', [ApiExampleController::class, 'getUsers']);
+		$router->get('/users/@id:[0-9]', [ApiExampleController::class, 'getUser']);
+		$router->post('/users/@id:[0-9]', [ApiExampleController::class, 'updateUser']);
 	});
-	
-}, [ SecurityHeadersMiddleware::class ]);
+
+}, [SecurityHeadersMiddleware::class]);
