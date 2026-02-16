17a1b4a3c7eeb73ff0e8129041d201af967194ac
Toavina-Andria <teoandriamonta@gmail.com>
2026-02-16 16:17:10 +0300

    création de l'entête

---
+++ app/views/partial/head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tableau de Bord - Gestion des Sinistres</title>
+    <link href="<?= $basepath ?>/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="<?= $basepath ?>/assets/css/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="<?= $basepath ?>/assets/css/dashboard.css">
+</head>
+<body class="bg-light"></body>
